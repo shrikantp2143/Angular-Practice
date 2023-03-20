a24e970db0ba2775e0189cad3ecbb2006a0b504f
shrikantp2143 <shrikantp2143@gmail.com>
2023-03-20 10:01:26 +0530

    using hostbinding to set color

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, Renderer2, ElementRef, OnInit, HostListener} from '@angular/core';
+import { Directive, Renderer2, ElementRef, OnInit, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @HostBinding('style.color') textColor = 'black'
   constructor(private renderer: Renderer2, private el: ElementRef) {
 
    }
@@ -14,11 +15,13 @@ export class BetterHighlightDirective implements OnInit {
    }
 
    @HostListener('mouseenter') mouseover() {
-    this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+    // this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+    this.textColor = 'red'
    }
 
    @HostListener('mouseleave') mouseleave() {
-    this.renderer.setStyle(this.el.nativeElement, 'color', 'black');
+    // this.renderer.setStyle(this.el.nativeElement, 'color', 'black');
+    this.textColor = 'blue';
    }
 
 }
